bc56786eb49eccdc602a91695751fba438b7884d
Luis Pérez <luis.perez@gatech.edu>
2014-07-20 23:21:26 -0600

    Move API key to private file and update gitignore

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 NUM_INGREDIENT_LISTS = 4
 NUM_INGREDIENTS = 2
-# TODO: probably wanna make this read the key from another file that's not checked in.
-API_KEY = 'X2p6hUG+5fgzfoTSGEDBiGsiYHRBE2ee1atd2fcppVhx9C8vtUUy4EXnjn/qDPeJ5Ty+4EItCf5mXZI8aMxMcQ=='
+with open('api_key') as file:
+	API_KEY = file.read()
 PROJECT = 'https://ussouthcentral.services.azureml.net/workspaces/76c97bb7c7f640719cd92db28794a965/services/3a865a07cfd94e25b18374d30a41fda7/score'
