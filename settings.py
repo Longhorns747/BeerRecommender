32868f6ddf34901a42794cb0962e5c188d52d050
Luis Pérez <luis.perez@gatech.edu>
2014-07-21 21:58:41 -0600

    Change project URL.

--- settings.py
+++ settings.py
@@ -3,4 +3,4 @@ NUM_INGREDIENTS = 2
 # Should add some exception handling with a message telling you that you're a dumbass and didn't fill out api_key
 with open('api_key') as file:
 	API_KEY = file.read()
-PROJECT = 'https://ussouthcentral.services.azureml.net/workspaces/76c97bb7c7f640719cd92db28794a965/services/3a865a07cfd94e25b18374d30a41fda7/score'
+PROJECT = 'https://ussouthcentral.services.azureml.net/workspaces/76c97bb7c7f640719cd92db28794a965/services/c4d6cda5f5854a4c9559d1d2e750d6bd/score'
