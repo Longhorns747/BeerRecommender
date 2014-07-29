00776be9faa8fa2b8cf2a960cdbdf29a6430a6ec
Luis Pérez <luis.perez@gatech.edu>
2014-07-29 00:12:32 -0600

    Add exception handling to settings

--- FlaskApplication/settings.py
+++ FlaskApplication/settings.py
@@ -1,8 +1,15 @@
 NUM_INGREDIENT_LISTS = 4
 NUM_INGREDIENTS = 2
 
-# Should add some exception handling with a message telling you that you're a dumbass and didn't fill out api_key
-with open('FlaskApplication/api_key') as f:
-    API_KEY = f.read()
+try:
+    with open('FlaskApplication/api_key') as f:
+        API_KEY = f.read()
+        if not API_KEY:
+            raise EOFError
+except (IOError, EOFError) as e:
+    print("You forgot to create an api_key file. Good job.")
+except EOFError:
+    print("You forgot to enter your api_key. Smart.")
+
 PROJECT = 'https://ussouthcentral.services.azureml' \
           '.net/workspaces/76c97bb7c7f640719cd92db28794a965/services/c4d6cda5f5854a4c9559d1d2e750d6bd/score'
